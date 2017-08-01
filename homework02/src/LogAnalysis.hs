81d1410f05a705e7399dc44e8328e18e9cfbeb58
Francisco Mena <menafrancisco@hotmail.com>
2017-08-01 08:47:43 -0500

    Homework2

--- homework02/src/LogAnalysis.hs
+++ homework02/src/LogAnalysis.hs
@@ -17,7 +17,7 @@ import           Log
 
 -- |
 --
--- >>> parseMessage "E 2 562 help help"
+-- >>> parseMessage "E 2 562 help help"parseMessage "E 2 562 help help"
 -- LogMessage (Error 2) 562 "help help"
 -- >>> parseMessage "I 29 la la la"
 -- LogMessage Info 29 "la la la"
@@ -45,8 +45,6 @@ parseMessage m = case messageCode of
 
 
 
-
-
 parse :: String -> [LogMessage]
 --parse :: String -> [String]
 --parse = undefined
@@ -67,49 +65,70 @@ parse n = xz :  parse rest
 -- Exercise 2
 ----------------------------------------------------------------------
 
--- |
---
+-- >>> insert (LogMessage Info 5 "Info5") (Node Leaf (LogMessage (Error 2) 2 "Error2") Leaf)
+-- Node Leaf (LogMessage Info 5 "Info5") (Node Leaf (LogMessage (Error 2) 2 "Error2") Leaf)
 -- >>>
 --
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = undefined
+--insert = undefined
+insert (Unknown _) messageTree = messageTree
+insert logMessage messageTree  = Node Leaf logMessage messageTree
 
 ----------------------------------------------------------------------
 -- Exercise 3
-----------------------------------------------------------------------
+---------------------------------------------whatWentWrong = undefined--------------------------
 
+-- >>> build [LogMessage Info 5 "Info5", LogMessage (Error 2) 2 "Error2"]
+-- Node Leaf (LogMessage Info 5 "Info5") (Node Leaf (LogMessage (Error 2) 2 "Error2") Leaf)
 -- |
 --
 -- >>>
 --
 
 build :: [LogMessage] -> MessageTree
-build = undefined
+--build = undefined
+build = foldr (Node Leaf) Leaf
+-- build [] = Leaf
+-- build (logMessage:logMessages) = Node Leaf logMessage (build logMessages)
 
 ----------------------------------------------------------------------
 -- Exercise 4
 ----------------------------------------------------------------------
 
+-- >>> inOrder $ Node Leaf (LogMessage Info 5 "Info5") (Node Leaf (LogMessage (Error 2) 2 "Error2") Leaf)
+-- >>> inOrder $ Node (Node Leaf (LogMessage (Error 7) 7 "Error7") Leaf) (LogMessage Info 5 "Info5") (Node Leaf (LogMessage (Error 2) 2 "Error2") Leaf)
+-- [LogMessage Info 5 "Info5",LogMessage (Error 2) 2 "Error2"]
 -- |
 --
 -- >>>
 --
 
 inOrder :: MessageTree -> [LogMessage]
-inOrder = undefined
+--inOrder = undefined
+inOrder Leaf = []
+inOrder (Node Leaf logMessage Leaf) = [logMessage]
+inOrder (Node Leaf logMessage messageTreeR) = logMessage : inOrder messageTreeR
+inOrder (Node messageTreeL logMessage Leaf) = inOrder messageTreeL ++ [logMessage]
+inOrder (Node messageTreeL logMessage messageTreeR) =  inOrder messageTreeL ++ [logMessage] ++ inOrder messageTreeR
 
 ----------------------------------------------------------------------
 -- Exercise 5
 ----------------------------------------------------------------------
 
+
+-- >>> whatWentWrong [LogMessage (Error 1) 1 "Error1", LogMessage (Error 2) 2 "Error2", LogMessage Info 5 "Info5"]
+-- ["Error1","Error2"]
 -- |
 --
 -- >>>
 --
 
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = undefined
+--whatWentWrong = undefined
+whatWentWrong [] = []
+whatWentWrong (LogMessage (Error _) _ message:lms) = message : whatWentWrong lms
+whatWentWrong (_:logMessages) = whatWentWrong logMessages
 
 ----------------------------------------------------------------------
 -- Exercise 6 (Optional)
