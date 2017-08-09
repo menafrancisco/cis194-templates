2d8e4293f85b6663b6009dfe674e43baf7f36a96
Francisco Mena <menafrancisco@hotmail.com>
2017-08-09 17:15:22 -0500

    Homework 3 exercise 3

--- homework03/test.hs
+++ homework03/test.hs
@@ -1,28 +1,8 @@
-----------------------------------------------------------------------
--- |
---
--- CIS 194 Spring 2013: Homework 03
---
-----------------------------------------------------------------------
+module Test where
 
-module Golf where
+import           Data.List
+import           Data.Maybe
 
-----------------------------------------------------------------------
--- Exercise 1
-----------------------------------------------------------------------
-
--- |
---
--- >>> skips "ABCD"
--- ["ABCD","BD","C","D"]
--- >>> skips "hello!"
--- ["hello!","el!","l!","l","o","!"]
--- >>> skips [1]
--- [[1]]
--- >>> skips [True, False]
--- [[True,False],[False]]
--- >>> skips []
--- []
 
 f ::[a] -> Int-> [a]
 f   []   _ = []
@@ -38,18 +18,7 @@ skips' _ [] = []
 skips' n l@(_:xs) =  f l m : skips' m xs
     where m =n+1
 
-----------------------------------------------------------------------
--- Exercise 2
-----------------------------------------------------------------------
 
--- |
---
--- >>> localMaxima [2,9,5,6,1]
--- [9,6]
--- >>> localMaxima [2,3,4,1,5]
--- [4]
--- >>> localMaxima [1,2,3,4,5]
--- []
 
 localMaxima :: [Integer] -> [Integer]
 localMaxima []=        []
@@ -58,16 +27,6 @@ localMaxima [_,_]=     []
 localMaxima (x:y:z:xs)= a ++ localMaxima (y:z:xs)
     where a= [y | y > x && y > z]
 
-----------------------------------------------------------------------
--- Exercise 3
-----------------------------------------------------------------------
-
--- |
---
--- >>> histogram [1,1,1,5]
--- " *        \n *        \n *   *    \n==========\n0123456789\n"
-import  Data.fromMaybe
-import  Data.List
 
 histogram :: [Integer] -> String
 histogram n = ss
@@ -92,4 +51,5 @@ fullfill n c s
             len = length s
             m = if n-len > 0 then n-len else 0
             aux= replicate m c
+
 
